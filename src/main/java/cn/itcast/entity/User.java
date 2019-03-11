093ef947d757a6a1820aeb69b6c2686fbb3ae3e9
eric <1479124685@qq.com>
2019-03-11 17:41:05 +0800

    演示一下 mybatis 的逆向工程项目

---
+++ src/main/java/cn/itcast/entity/User.java
@@ -0,0 +1,55 @@
+package cn.itcast.entity;
+
+import java.util.Date;
+
+public class User {
+    private Integer uid;
+
+    private String username;
+
+    private Date birthday;
+
+    private String sex;
+
+    private String address;
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex == null ? null : sex.trim();
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address == null ? null : address.trim();
+    }
+}
---
+++ src/main/java/cn/itcast/generator/MyGenerator.java
@@ -0,0 +1,33 @@
+package cn.itcast.generator;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+public class MyGenerator {
+	public void generator() throws Exception{
+		List<String> warnings = new ArrayList<String>();
+		boolean overwrite = true;
+		//指定 逆向工程配置文件, 注意是项目路径下，不是类路径下
+		File configFile = new File("generatorConfig.xml"); 
+		ConfigurationParser cp = new ConfigurationParser(warnings);
+		Configuration config = cp.parseConfiguration(configFile);
+		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
+				callback, warnings);
+		myBatisGenerator.generate(null);
+	} 
+	public static void main(String[] args) throws Exception {
+		try {
+			MyGenerator generatorSqlmap = new MyGenerator();
+			generatorSqlmap.generator();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}	
+	}
+}
